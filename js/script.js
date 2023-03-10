11721897d6109f478837018b47fbb46ff1c04334
samibhojani <msammi96@gmail.com>
2023-03-10 07:49:05 -0600

    setting up folders and files structure

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// <!DOCTYPE html>
+// <html>
+// <body>
+// <h1>The Document Object</h1>
+// <h2>The write() Method</h2>
+// <p>Write some HTML elements directly to the HTML output:</p>
+// <input id="myemail" type="number"/> <button onclick="renderResult()" >Submit</button>
+// <script>
+// function renderResult(){
+// var value = document.getElementById("myemail").value
+// if(value.toString().length > 2){
+// for(var i = 1; i < 10 ; i++){
+// const result = 5 + "X" + i + "=" + 5 * i + "<br/>"
+//  document.write(result)
+// if(i === 9)
+// return
+// }
+// }
+// for(var i = 1; i < 10 ; i++){
+// const result = value + "X" + i + "=" + value * i + "<br/>"
+// }
+// }
+// </script>
+// </body>
+// </html>
